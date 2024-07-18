420f0aafde18a48b3e545ee268a944549813e057
cemil3535 <cemilozcan35@gmail.com>
2024-07-18 18:37:01 +0300

    Add project files.

---
+++ PratikIfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Kullaninican sayi alnimasi.
+Console.Write("Lutfen bir sayi giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+// Kullanicidan alinan sayinin 10'dan buyuk,kucuk,esit olup olmadiginin kontrol edilmesi.
+if (sayi > 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan buyuktur.");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan kucuktur.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi 10'a esittir.");
+}
+
+// Kullanicidan alinan sayinin tek mi?, cift mi? oldugunun kontrol edilmesi.
+if (sayi % 2 ==0)
+{
+    Console.WriteLine("Girilen sayi cifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi tektir.");
+}
+
+
